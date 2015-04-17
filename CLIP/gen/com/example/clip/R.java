24d026f69d0db9d411342f2051817f7d119b6690
randythiencao <randythiencao@yahoo.com>
2015-04-17 01:23:26 -0500

    Merging in Education Buttons
    just some buttons

--- CLIP/gen/com/example/clip/R.java
+++ CLIP/gen/com/example/clip/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
+        public static final int Horizontal=0x7f040002;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Example customization of dimensions originally defined in res/values/dimens.xml
@@ -25,18 +26,45 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int currEd=0x7f080001;
+        public static final int financial=0x7f080004;
+        public static final int gradPlan=0x7f080002;
+        public static final int gradSchool=0x7f080003;
+        public static final int others=0x7f080005;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_curr_education=0x7f030000;
+        public static final int activity_financial=0x7f030001;
+        public static final int activity_grad_plan=0x7f030002;
+        public static final int activity_grad_schools=0x7f030003;
+        public static final int activity_main=0x7f030004;
+        public static final int activity_others=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int curr_education=0x7f070000;
+        public static final int financial=0x7f070001;
+        public static final int grad_plan=0x7f070002;
+        public static final int grad_schools=0x7f070003;
+        public static final int main=0x7f070004;
+        public static final int others=0x7f070005;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int cur_Ed=0x7f050004;
+        public static final int education_title=0x7f050003;
+        public static final int financial_ed=0x7f050007;
+        public static final int grad_Plan=0x7f050005;
+        public static final int grad_school=0x7f050006;
         public static final int hello_world=0x7f050001;
+        public static final int other=0x7f050008;
+        public static final int title_activity_curr_education=0x7f050009;
+        public static final int title_activity_financial=0x7f05000c;
+        public static final int title_activity_grad_plan=0x7f05000a;
+        public static final int title_activity_grad_schools=0x7f05000b;
+        public static final int title_activity_others=0x7f05000d;
     }
     public static final class style {
         /** 
--- CLIP/src/com/example/clip/MainActivity.java
+++ CLIP/src/com/example/clip/MainActivity.java
@@ -1,16 +1,78 @@
 package com.example.clip;
 
+//import com.example.example.R;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button currEducation;
+	Button gradPlan;
+	Button gradSchools;
+	Button financial;
+	Button others;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		currEducation = (Button) findViewById(R.id.currEd);
+		currEducation.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this, CurrEducation.class);
+				startActivity(i);
+			//	finish();
+	         }
+		});
+		
+		gradPlan = (Button) findViewById(R.id.gradPlan);
+		gradPlan.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this, GradPlan.class);
+				startActivity(i);
+			//	finish();
+	         }
+		});
+		
+		gradSchools = (Button) findViewById(R.id.gradSchool);
+		gradSchools.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this, GradSchools.class);
+				startActivity(i);
+			//	finish();
+	         }
+		});
+		
+		financial = (Button) findViewById(R.id.financial);
+		financial.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this, Financial.class);
+				startActivity(i);
+			//	finish();
+	         }
+		});
+		
+		others = (Button) findViewById(R.id.others);
+		others.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this, Others.class);
+				startActivity(i);
+			//	finish();
+	         }
+		});
 	}
 
 	@Override
