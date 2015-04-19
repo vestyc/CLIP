fe553f07bb732efba85a3958d73dba8b671b5fc2
randythiencao <randythiencao@yahoo.com>
2015-04-19 02:03:31 -0500

    Added Login Page
    added text fields to check file created and content, working

--- CLIP/src/com/example/clip/MainActivity.java
+++ CLIP/src/com/example/clip/MainActivity.java
@@ -1,23 +1,90 @@
 package com.example.clip;
 
-import com.example.clip.career.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+
 
 public class MainActivity extends Activity {
 
+	EditText userName;
+	EditText passWord;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Intent i = new Intent(MainActivity.this, CareerMenu.class);
-		startActivity(i);
+		userName = (EditText)findViewById(R.id.userName);
+		passWord = (EditText)findViewById(R.id.passWord);
+
 	}
 
+	
+	public void LoginButton(View v) {
+		// add-write text into file
+		try {
+			FileOutputStream fileout=openFileOutput(userName.getText().toString()+".txt", MODE_PRIVATE);
+			OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+			outputWriter.write(userName.getText().toString());
+			outputWriter.write("\n");
+			outputWriter.write(passWord.getText().toString());
+			outputWriter.close();
+			
+			//display file saved message
+			Toast.makeText(getBaseContext(), "File saved successfully!",
+			Toast.LENGTH_SHORT).show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public void ReadBtn(View v) {
+		//reading text from file
+		try {
+			FileInputStream fileIn=openFileInput(userName.getText().toString()+".txt");
+			InputStreamReader InputRead= new InputStreamReader(fileIn);
+			
+			char[] inputBuffer= new char[100];
+			String s="";
+			int charRead;
+			
+			while ((charRead=InputRead.read(inputBuffer))>0) {
+				// char to string conversion
+				String readstring=String.copyValueOf(inputBuffer,0,charRead);
+				s +=readstring;					
+			}
+			InputRead.close();
+			Toast.makeText(getBaseContext(), s,Toast.LENGTH_SHORT).show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
