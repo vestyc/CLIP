231d4720eea0c737312c3786713da0e86d854a6d
Johnny <tranjohnny64@gmail.com>
2015-04-20 09:45:01 -0500

    Career Button
    added a career button for Entry activity

--- CLIP/src/com/example/clip/Entry.java
+++ CLIP/src/com/example/clip/Entry.java
@@ -1,16 +1,36 @@
 package com.example.clip;
 
+import com.example.clip.career.CareerGoal;
+import com.example.clip.career.CareerMenu;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class Entry extends Activity {
 
+	Button career;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_entry);
+		
+		career = (Button) findViewById(R.id.button_career);
+		
+		career.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				
+				Intent i = new Intent(Entry.this, CareerMenu.class);
+				startActivity(i);
+	         }
+		});
 	}
 
 	@Override
