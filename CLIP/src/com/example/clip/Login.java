b1a34709560259b66b213f372ac9e23659bf0d9b
randythiencao <randythiencao@yahoo.com>
2015-04-20 01:42:49 -0500

    Login / Register Button

--- CLIP/src/com/example/clip/Login.java
+++ CLIP/src/com/example/clip/Login.java
@@ -25,10 +25,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import 	android.view.Gravity;
 
 
-
-public class MainActivity extends Activity {
+public class Login extends Activity {
 
 	EditText userName;
 	EditText passWord;
@@ -38,97 +38,93 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_login);
 		
 		userName = (EditText)findViewById(R.id.userName);
 		passWord = (EditText)findViewById(R.id.passWord);
+		userName.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+		passWord.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
 		myData = new DataStorage(getApplicationContext());
 
 	}
 
 	
-	public void LoginButton(View v) {
+	public void LoginButton(View v) 
+	{
 		// add-write text into file
 		
-		try {
+		try 
+		{
 			if(myData.checkUserName(userName.getText().toString()))
 			{
 				if(myData.getPassword(userName.getText().toString()).equals(passWord.getText().toString()))
 				{
+					Toast.makeText(getBaseContext(), "Logged in Successfully", Toast.LENGTH_SHORT).show();
 					Intent i = new Intent(this, Entry.class);
 					startActivity(i);
+				}
+				else
+				{
+					Toast.makeText(getBaseContext(), "Invalid Username/Password", Toast.LENGTH_SHORT).show();
 				}
 			}
 			else
 			{
-				FileOutputStream fileout=openFileOutput(userName.getText().toString()+".txt", MODE_PRIVATE);
-				OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
-				outputWriter.write(passWord.getText().toString());
-				outputWriter.write("\n");
-				outputWriter.close();
-				
-				//display file saved message
-				Toast.makeText(getBaseContext(), "File saved successfully!",
-				Toast.LENGTH_SHORT).show();
-			}
-			/*FileOutputStream fileout=openFileOutput(userName.getText().toString()+".txt", MODE_PRIVATE);
+				Toast.makeText(getBaseContext(), "Invalid Username/Password", Toast.LENGTH_SHORT).show();
+			}	
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}	
+	}
+	
+	public void Register(View v)
+	{
+		
+		try 
+		{
+			FileOutputStream fileout=openFileOutput(userName.getText().toString()+".txt", MODE_PRIVATE);
 			OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
 			outputWriter.write(passWord.getText().toString());
 			outputWriter.write("\n");
 			outputWriter.close();
 			
 			//display file saved message
-			Toast.makeText(getBaseContext(), "File saved successfully!",
-			Toast.LENGTH_SHORT).show();*/
+			Toast.makeText(getBaseContext(), "Registered Successfully",
+			Toast.LENGTH_SHORT).show();
+			
 		
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
-		}	
+		} 
+		finally
+		{
+			Intent i = new Intent(this, Entry.class);
+			startActivity(i);
+		}
 	}
-	
-	
-	// DOUBLE CHECK=========================================
-	public void ReadBtn(View v) {
-		//reading text from file
-		myData.getPassword(userName.getText().toString());
-		//Toast.makeText(getBaseContext(), myData.getPassword(userName.getText().toString()),
-			//	Toast.LENGTH_SHORT).show();
-				//return;
-		/*try {
-			FileInputStream fileIn=openFileInput(userName.getText().toString()+".txt");
-			InputStreamReader InputRead= new InputStreamReader(fileIn);
-			
-			char[] inputBuffer= new char[100];
-			String s="";
-			int charRead;
-			
-			while ((charRead=InputRead.read(inputBuffer))>0) {
-				// char to string conversion
-				String readstring=String.copyValueOf(inputBuffer,0,charRead);
-				s +=readstring;					
-			}
-			InputRead.close();
-			Toast.makeText(getBaseContext(), s,Toast.LENGTH_SHORT).show();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}*/
-	}
+
 	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_settings) 
+		{
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
