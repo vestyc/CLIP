4983c08a2a3f0cd4aa7ae300b1f412bac34b6629
Johnny <tranjohnny64@gmail.com>
2015-04-23 11:31:55 -0500

    (JobApp) Working on add/edit

--- CLIP/src/com/example/clip/career/CareerJobAppEdit.java
+++ CLIP/src/com/example/clip/career/CareerJobAppEdit.java
@@ -9,13 +9,57 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.view.View;
+import android.content.Intent;
 
-public class CareerJobAppEdit extends Activity {
+public class CareerJobAppEdit extends Activity implements OnItemSelectedListener {
 
+	ArrayAdapter<CharSequence> spinnerAdapter;
+	EditText jobName;
+	Button saveButton;
+	Spinner status;
+
+	String name;
+	String[] data;
+	
+	Intent iReturn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_career_job_app_edit);
+		
+		iReturn = new Intent();
+		
+		jobName = (EditText) findViewById(R.id.jobName);
+		saveButton = (Button) findViewById(R.id.job_buttonSave);
+		
+		status = (Spinner) findViewById(R.id.job_statusSpinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		spinnerAdapter = ArrayAdapter.createFromResource(this,
+		        R.array.job_status, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		status.setAdapter(spinnerAdapter);
+		
+		//edit option
+		if(getIntent().getStringExtra("name") != null) {
+			
+			//Display current jobName
+			jobName.setText(getIntent().getStringExtra("name"));
+			iReturn.putExtra("oldName", jobName.getText().toString());
+			
+			// data = {dateApplied, appStatus, comments}
+			data = getIntent().getStringArrayExtra("data");
+			//set current date applied
+			
+			//--------
+			
+			
+		}
 	}
 
 	@Override
@@ -36,4 +80,16 @@ public class CareerJobAppEdit extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, 
+            int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+	@Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
 }
