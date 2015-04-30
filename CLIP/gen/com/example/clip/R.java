66ef94596ca53bcbf0fe5cba58fc223f65a34116
randythiencao <randythiencao@yahoo.com>
2015-04-30 11:47:28 -0500

    added icon

--- CLIP/gen/com/example/clip/R.java
+++ CLIP/gen/com/example/clip/R.java
@@ -33,7 +33,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int loginbackground=0x7f020001;
+        public static final int icon=0x7f020001;
+        public static final int loginbackground=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090001;
